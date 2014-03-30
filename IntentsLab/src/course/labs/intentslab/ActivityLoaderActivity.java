d1e799bf4b81674316a4fa6c75223955db32402e
HugoBex <hugobex.dev@gmail.com>
2014-03-30 19:00:02 -0300

    Week 3 Implementation

--- IntentsLab/src/course/labs/intentslab/ActivityLoaderActivity.java
+++ IntentsLab/src/course/labs/intentslab/ActivityLoaderActivity.java
@@ -66,10 +66,9 @@ public class ActivityLoaderActivity extends Activity {
 		Log.i(TAG,"Entered startExplicitActivation()");
 		
 		// TODO - Create a new intent to launch the ExplicitlyLoadedActivity class
-		
+		Intent intent = new Intent(this, ExplicitlyLoadedActivity.class);
 		// TODO - Start an Activity using that intent and the request code defined above
-
-
+		startActivityForResult(intent, GET_TEXT_REQUEST_CODE);
 	}
 
 	// Start a Browser Activity to view a web page or its URL
@@ -77,17 +76,17 @@ public class ActivityLoaderActivity extends Activity {
 	private void startImplicitActivation() {
 
 		Log.i(TAG, "Entered startImplicitActivation()");
-
 		// TODO - Create a base intent for viewing a URL 
 		// (HINT:  second parameter uses parse() from the Uri class)
-		
-		
+		Intent baseIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URL));
+    
 		// TODO - Create a chooser intent, for choosing which Activity
 		// will carry out the baseIntent. Store the Intent in the 
 		// chooserIntent variable below. HINT: using the Intent class' 
 		// createChooser())
-		
+			
 		Intent chooserIntent = null;
+		chooserIntent = Intent.createChooser(baseIntent, CHOOSER_TEXT);
 
 		Log.i(TAG,"Chooser Intent Action:" + chooserIntent.getAction());
 		// TODO - Start the chooser Activity, using the chooser intent
@@ -100,10 +99,14 @@ public class ActivityLoaderActivity extends Activity {
 
 		Log.i(TAG, "Entered onActivityResult()");
 		
-		// TODO - Process the result only if this method received both a
+		if ( requestCode != GET_TEXT_REQUEST_CODE || resultCode != RESULT_OK )
+			return;
+					
+		// TODO - Process the result only if this method received both a		
 		// RESULT_OK result code and a recognized request code
 		// If so, update the Textview showing the user-entered text.
-
-
+		
+		String resultData = data.getStringExtra("returnValue");
+		mUserTextView.setText(resultData);
 	}
 }
